9ba4c65f3b75ec28e03c7152f642849318d2abc3
fanrong.zfr <fanrong.zfr@alibaba-inc.com>
2018-07-17 02:00:14 +0800

    last day

--- jddata/app/src/main/java/com/example/jddata/util/StorageUtil.kt
+++ jddata/app/src/main/java/com/example/jddata/util/StorageUtil.kt
@@ -16,7 +16,7 @@ class StorageUtil {
         @JvmStatic fun outputDatabaseDatas() {
             BusHandler.instance.singleThreadExecutor.execute(Runnable {
                 val sb = StringBuilder()
-                sb.append("编号,动作组id,日期,创建时间,账号,gps位置,ip归属地,采集数据间隔,bi点位,页面位置,标题,副标题,产品,价格/秒杀价,原价/京东价,描述,数量,排行榜城市,排行榜标签,收藏数,看过数,评论,好评率,京东秒杀场次\n")
+                sb.append("编号,imei,动作组id,日期,创建时间,账号,gps位置,ip归属地,采集数据间隔,bi点位,商品位置,标题,副标题,产品,价格/秒杀价,原价/京东价,描述,数量,排行榜城市,排行榜标签,收藏数,看过数,评论数,好评率,京东秒杀场次\n")
                 MainApplication.getContext().database.use {
                     transaction {
                         val builder = select(GlobalInfo.TABLE_NAME)
