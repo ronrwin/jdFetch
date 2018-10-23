30645315f012324ccdbe0d2b1b8af044b76d0332
fanrong.zfr <fanrong.zfr@alibaba-inc.com>
2018-10-23 18:23:04 +0800

    命令改成enum方式

--- jddata/app/src/main/java/com/example/jddata/service/ServiceCommand.kt
+++ jddata/app/src/main/java/com/example/jddata/service/ServiceCommand.kt
@@ -1,55 +1,54 @@
 package com.example.jddata.service
 
-object ServiceCommand {
-    @JvmField val CLICK_SEARCH = 1
-    @JvmField val CATEGORY = 2
-    @JvmField val HOME_SCROLL = 3
-    @JvmField val INPUT = 4
-    @JvmField val SEARCH = 5
-    @JvmField val SEARCH_DATA = 6
-    @JvmField val AGREE = 7
-    @JvmField val CLOSE_AD = 8
-    @JvmField val GO_BACK = 9
-    @JvmField val CART_TAB = 10
-    @JvmField val CART_SCROLL = 11
-    @JvmField val HOME_TAB = 12
-    @JvmField val LEADERBOARD = 13
-    @JvmField val HOME_JD_KILL = 14
-    @JvmField val JD_KILL_SCROLL = 15
-    @JvmField val WORTH_BUY = 16
-    @JvmField val WORTH_BUY_SCROLL = 17
-    @JvmField val NICE_BUY = 18
-    @JvmField val NICE_BUY_SCROLL = 19
-    @JvmField val HOME_BRAND_KILL = 20
-    @JvmField val HOME_BRAND_KILL_SCROLL = 21
-    @JvmField val HOME_TYPE_KILL = 22
-    @JvmField val SCREENSHOT = 23
-    @JvmField val NICE_BUY_SELECT = 24
-    @JvmField val NICE_BUY_DETAIL = 25
-    @JvmField val BRAND_SELECT = 26
-    @JvmField val BRAND_DETAIL = 27
-    @JvmField val HOME_TYPE_KILL_SCROLL = 28
-    @JvmField val TYPE_SELECT = 29
-    @JvmField val TYPE_DETAIl = 30
-    @JvmField val LEADERBOARD_TAB = 31
-    @JvmField val DMP_CLICK = 32
-    @JvmField val DMP_TITLE = 33
-    @JvmField val HOME_BRAND_KILL_AND_SHOP = 34
-    @JvmField val BRAND_SELECT_RANDOM = 35
-    @JvmField val BRAND_DETAIL_RANDOM_SHOP = 36
-    @JvmField val PRODUCT_BUY = 37
-    @JvmField val PRODUCT_CONFIRM = 38
-    @JvmField val SEARCH_DATA_RANDOM_BUY = 39
-    @JvmField val DMP_FIND_PRICE = 40
-    @JvmField val LEADERBOARD_CONTENT = 41
-    @JvmField val LEADERBOARD_SELECT_TYPE = 42
-    @JvmField val SEACH_CLICK = 43
-    @JvmField val JD_KILL_CLICK = 44
-    @JvmField val JD_KILL_BUY = 45
-    @JvmField val CAPTURE_SCAN = 46
-    @JvmField val SCAN_CLBUM = 47
-    @JvmField val SCAN_PIC = 48
-    @JvmField val JD_FIND_REMIND_ME = 49
-    @JvmField val JD_FIND_CLICK_COMING = 50
-    @JvmField val PRODUCT_DETAIL = 51
+enum class ServiceCommand {
+    CLICK_SEARCH,
+    HOME_SCROLL,
+    INPUT,
+    SEARCH,
+    SEARCH_DATA,
+    AGREE,
+    CLOSE_AD,
+    GO_BACK,
+    CART_TAB,
+    CART_SCROLL,
+    HOME_TAB,
+    LEADERBOARD,
+    HOME_JD_KILL,
+    JD_KILL_SCROLL,
+    WORTH_BUY,
+    WORTH_BUY_SCROLL,
+    NICE_BUY,
+    NICE_BUY_SCROLL,
+    HOME_BRAND_KILL,
+    HOME_BRAND_KILL_SCROLL,
+    HOME_TYPE_KILL,
+    SCREENSHOT,
+    NICE_BUY_SELECT,
+    NICE_BUY_DETAIL,
+    BRAND_SELECT,
+    BRAND_DETAIL,
+    HOME_TYPE_KILL_SCROLL,
+    TYPE_SELECT,
+    TYPE_DETAIl,
+    LEADERBOARD_TAB,
+    DMP_CLICK,
+    DMP_TITLE,
+    HOME_BRAND_KILL_AND_SHOP,
+    BRAND_SELECT_RANDOM,
+    BRAND_DETAIL_RANDOM_SHOP,
+    PRODUCT_BUY,
+    PRODUCT_CONFIRM,
+    SEARCH_DATA_RANDOM_BUY,
+    DMP_FIND_PRICE,
+    LEADERBOARD_CONTENT,
+    LEADERBOARD_SELECT_TYPE,
+    SEACH_CLICK,
+    JD_KILL_CLICK,
+    JD_KILL_BUY,
+    CAPTURE_SCAN,
+    SCAN_CLBUM,
+    SCAN_PIC,
+    JD_FIND_REMIND_ME,
+    JD_FIND_CLICK_COMING,
+    PRODUCT_DETAIL
 }
